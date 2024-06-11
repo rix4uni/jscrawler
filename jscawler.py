a05f31c7fb42c8f1c0c1a58ab806554dfea7d6a9
Bhagirath Saxena <72344025+rix4uni@users.noreply.github.com>
2024-06-11 13:59:39 +0530

    Update jscawler.py

--- jscawler.py
+++ jscawler.py
@@ -3,26 +3,58 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from urllib.parse import urljoin
+import argparse
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+# Define the version
+__version__ = "v0.0.1"  # Current Version of jscrawler
+
+parser = argparse.ArgumentParser(description='jscrawler - Fetches JavaScript links from a list of URLs or live subdomains.')
+parser.add_argument('--timeout', default=5, help='Timeout (in seconds) for http client (default 15)')
+parser.add_argument('--complete', action='store_true', help='Get Complete URL (default false)')
+parser.add_argument('-o', '--output', help='Output file to save results')
+parser.add_argument('-v', '--verbose', action='store_true', help='Display info of what is going on')
+parser.add_argument('--version', action='version', version='%(prog)s ' + __version__)
+args = parser.parse_args()
+
 for base_url in sys.stdin:
-	base_url = base_url.rstrip()
+    base_url = base_url.rstrip()
 
-	response = requests.get(base_url, timeout=5, verify=False)
-	soup = BeautifulSoup(response.text, 'html.parser')
+    try:
+        response = requests.get(base_url, timeout=int(args.timeout), verify=False)
+        if args.verbose:
+            print(f"Processing URL: {base_url}")
+    except requests.exceptions.Timeout:
+        if args.verbose:
+            print(f"Timeout occurred while fetching: {base_url}")
+        continue
 
-	# # Convert the BeautifulSoup object to a string
-	soup_str = str(soup)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-	# # Use the regex pattern to find all matches
-	links = re.findall(r"['\"]([^'\"]*\.js[^'\"]*)['\"]", soup_str)
+    # # Convert the BeautifulSoup object to a string
+    soup_str = str(soup)
 
-	# Filter and print only those links that contain .js
-	for link in links:
-	    if '.js' in link:
-	        # Remove surrounding quotes
-	        link = link.strip('\'"')
-	        # Convert relative URLs to absolute URLs
-	        full_url = urljoin(base_url, link)
-	        print(full_url)
+    # # Use the regex pattern to find all matches
+    links = re.findall(r"['\"]([^'\"]*\.js[^'\"]*)['\"]", soup_str)
+
+    # Filter and print only those links that contain .js
+    for link in links:
+        if '.js' in link:
+            # Remove surrounding quotes
+            link = link.strip('\'"')
+
+            if args.complete:
+                # Convert relative URLs to absolute URLs
+                full_url = urljoin(base_url, link)
+                print(full_url)
+
+                if args.output:
+                    with open(args.output, 'a') as file:
+                        file.write(full_url + '\n')
+            else:
+                print(link)
+
+                if args.output:
+                    with open(args.output, 'a') as file:
+                        file.write(link + '\n')
